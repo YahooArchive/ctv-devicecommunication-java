d736b8061549b7314fcbf26b133d099dc18220d1
Jim Cortez <jim@jimcortez.com>
2012-05-24 11:40:15 -0700

    Updating Copyrights

--- examples/VideoListExample/VideoListExample.java
+++ examples/VideoListExample/VideoListExample.java
@@ -1,24 +1,24 @@
 /*******************************************************************************
- * Copyright (c) 2011, Yahoo! Inc.
+ * Copyright (c) 2012, Yahoo! Inc.
  * All rights reserved.
  *
  * Redistribution and use of this software in source and binary forms,
  * with or without modification, are permitted provided that the following
  * conditions are met:
  *
- * * Redistributions of source code must retain the above
- *   copyright notice, this list of conditions and the
- *   following disclaimer.
+ *  * Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the
+ *    following disclaimer.
  *
- * * Redistributions in binary form must reproduce the above
- *   copyright notice, this list of conditions and the
- *   following disclaimer in the documentation and/or other
- *   materials provided with the distribution.
+ *  * Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the
+ *    following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
  *
- * * Neither the name of Yahoo! Inc. nor the names of its
- *   contributors may be used to endorse or promote products
- *   derived from this software without specific prior
- *   written permission of Yahoo! Inc.
+ *  * Neither the name of Yahoo! Inc. nor the names of its
+ *    contributors may be used to endorse or promote products
+ *    derived from this software without specific prior
+ *    written permission of Yahoo! Inc.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
